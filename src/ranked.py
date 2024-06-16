79876edaa8a2bd96a6f3ca77e79fa57681c3186d
Gargant <gargant0373@gmail.com>
2024-06-16 19:37:52 +0200

    got ranked.py script to work

--- src/ranked.py
+++ src/ranked.py
@@ -3,12 +3,12 @@ import sys
 import os
 import traceback
 import csv
-import re
 from pydantic import BaseModel, Field
 from dspy import InputField, OutputField, TypedChainOfThought
 from dotenv import load_dotenv
+import re
 
-from schwartz import ValueInformation, RubricInformation, schwartz_values, rubric
+from schwartz import ValueInformation, schwartz_values, rubric, SchwartzValues
 
 from config import setup_logging, load_lyrics
 
@@ -43,60 +43,52 @@ results_dir = os.getenv('RESULTS_DIR')
 
 # Create the directory if it doesn't exist
 os.makedirs(results_dir, exist_ok=True)
-
-pattern = re.compile(r'ratings-\d+\.csv')
+pattern = re.compile(r'ranked-\d+\.csv')
 count = len([name for name in os.listdir(results_dir) if pattern.match(name)])
 
-result_file_path = os.path.join(results_dir, f"ratings-{count}.csv")
-weighted_file_path = os.path.join(results_dir, f"weighted-{count}.csv")
+result_file_path = os.path.join(results_dir, f"ranked-{count}.csv")
 
-write_header_if_empty(result_file_path, header)
-write_header_if_empty(weighted_file_path, header)
 
 # Create output class for answers
 # The model will try to output a JSON object with the 
 # following constraints
 class OutputScore(BaseModel):
-    score: int = Field(
-        ge=1,
-        le=5,
-        description="An integer between 1 and 5. Refer to the score rubric"
+    score: list[str] = Field(
+        description="""
+                Array of Schwartz values ranked by how important they are
+                according to the lyrics.
+                """
     )
     confidence: float = Field(
         ge=0.,
         le=1.,
         description="The confidence for the score, as a float from 0. to 1."
     )
-    feedback: str = Field(
-        description="""
-            Write a detailed feedback that assesses the quality of the
-            response strictly based on the given score rubric, not
-            evaluating in general. Include specific examples and
-            reasons for which the score was chosen
-        """
-    )
+    #feedback: str = Field(
+    #    description="""
+    #        Write a detailed feedback that assesses the quality of the
+    #        response strictly based on the given ranking, not
+    #        evaluating in general. Include specific examples and
+    #        reasons for which the score was chosen
+    #    """
+    #)
 
 # Main DSPY singature
 # Specifies what values are passed as inputs and what is 
 # expected as output
 class GenerateScore(dspy.Signature):
     """
-    Using the Scwhartz Theory of basic Human Values, give a score, an
-    integer from 1-5, according to the rubric, based on how important
-    the given value is according to the lyrics. Multiple values of 5
-    is a sign that something is off.
+    Using the Scwhartz Theory of basic Human Values, rank all the values
+    in the order of how important they are according to the lyrics.
     Then, on the scale of 0-1 tell how confident you are about the
     answer.
-    Lastly, provide a feedback stating why you chose the score.
     After "Output:" nothing should follow except the required JSON object.
     Your response should contain nothing besides the JSON object.
     """
 
-    value: ValueInformation = InputField()
+    values: SchwartzValues = InputField()
 
     lyrics: str = InputField(desc="lyrics to be rated")
-
-    score_rubric: RubricInformation = InputField()
 
     output: OutputScore = OutputField()
 
@@ -114,27 +106,21 @@ for l in lyrics:
 
     outs = []
     scores = []
-    confidence_scores = []
+    try: 
+        result = score_generator(
+                    values=schwartz_values,
+                    lyrics=lyi.lyrics,
+                    )
 
-    for val in schwartz_values.values:
-        logger.info(f"Assesing {val.value}...")
-        try:
-            result = score_generator(
-                value=val,
-                lyrics=lyi.lyrics,
-                score_rubric=rubric
-            )
-
-            outs.append(result.output)
-            scores.append(result.output.score)
-            confidence_scores.append(result.output.score * result.output.confidence)
-            logger.info(f"Finished assesing {val.value}: {result.output.score} (confidence: {result.output.confidence})")
-            logger.debug(f"Feedback: {result.output.feedback}")
-        except Exception:
-            logger.error(traceback.format_exc())
-        finally:
-            with open(os.devnull, "w") as sys.stdout:
-                logger.debug(f"Prompt: {lm.inspect_history(n=1)}")
+        outs.append(result.output)
+        scores.append(result.output.score)
+    
+        logger.info(f"Finished assesing: {result.output.score} (confidence: {result.output.confidence})")
+        #logger.debug(f"Feedback: {result.output.feedback}")
+    except Exception:
+        logger.error(traceback.format_exc())
+    finally:
+        with open(os.devnull, "w") as sys.stdout:
+            logger.debug(f"Prompt: {lm.inspect_history(n=1)}")
 
     append_to_csv(result_file_path, [lyi.mxm_id] + scores)
-    append_to_csv(weighted_file_path, [lyi.mxm_id] + confidence_scores)
